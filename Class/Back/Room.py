111110e35f8c8825aa74cd3166c1da8854859732
hugo-esquer <hugo.esquer@laplateforme.io>
2024-02-16 14:49:11 +0100

    User and Room classes

---
+++ Class/Back/Room.py
@@ -0,0 +1,37 @@
+from Db import Db
+
+class Room:
+    def __init__(self):
+        self.db = Db(host="localhost", user="root", password="root", database="db_discord")
+
+    def persmissions(self, id, roomId):
+        query = "SELECT * FROM permissions WHERE id_affiliate_user = %s"
+        values = (id,)
+        permissions = self.db.fetch(query, values)
+        for permission in permissions:
+            if permission["id_affiliate_canal"] == roomId:
+                return True
+        return False
+    
+    def add_user(self, id, roomId, admin = False):
+        query = "INSERT INTO permissions (id_affiliate_user, id_affiliate_canal, admin) VALUES (%s, %s, %s)"
+        values = (id, roomId, admin)
+        self.db.execute(query, values)
+
+    def remove_user(self, id, roomId):
+        query = "DELETE FROM permissions WHERE id_affiliate_user = %s AND id_affiliate_canal = %s"
+        values = (id, roomId)
+        self.db.execute(query, values)
+
+    def get_users(self, roomId):
+        query = "SELECT * FROM permissions WHERE id_affiliate_canal = %s"
+        values = (roomId,)
+        return self.db.fetch(query, values)
+    
+    def get_chat_rooms(self):
+        query = "SELECT * FROM chat_room WERE canal_type = text"
+        return self.db.fetch(query)
+    
+    def get_voice_rooms(self):
+        query = "SELECT * FROM chat_room WERE canal_type = voice"
+        return self.db.fetch(query)
