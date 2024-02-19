bde83a972e9dafb2d03b4d9279df2154b1aba2a8
hugo-esquer <hugo.esquer@laplateforme.io>
2024-02-19 12:51:49 +0100

    add connection class and correction post class

--- Class/Back/Post.py
+++ Class/Back/Post.py
@@ -5,17 +5,17 @@ class Post:
         self.db = Db(host='localhost', user='root', password='root', db='db_discord')
 
     def send_post(self, user_id, content, affiliate_chanel_id):
-        query = self.db.query(f"INSERT INTO posts (body, user_id, id_affiliate_chanel) VALUES (%s, %s, %s)")
+        query = "INSERT INTO posts (body, user_id, id_affiliate_chanel) VALUES (%s, %s, %s)"
         value = (content, user_id, affiliate_chanel_id)
         self.db.execute(query, value)
 
     def get_post(self, id_room):
-        query = self.db.query("SELECT * FROM posts WHERE id_affiliate_chanel = %s")
+        query = "SELECT * FROM posts WHERE id_affiliate_chanel = %s"
         value = (id_room,)
         return self.db.fetch(query, value)
     
     def get_post_by_id(self, id_post):
-        query = self.db.query("SELECT * FROM posts WHERE id = %s")
+        query = "SELECT * FROM posts WHERE id = %s"
         value = (id_post,)
         return self.db.fetch(query, value)
     
---
+++ Class/Back/connection.py
@@ -0,0 +1,25 @@
+from Db import Db
+from User import User
+import bcrypt
+
+class connection(User):
+    def __init__(self):
+        self.db = Db(host='localhost', user='root', password='root', db='db_discord')
+
+    def check_mail(self, mail): # check if the mail is already in the database
+        query = "SELECT mail FROM user"
+        list_mail = self.db.fetch(query)
+        if mail in list_mail:
+            return True
+        else:
+            return False
+        
+    def verify_password(self, password, hashed_password): # verify if the password is correct
+        return bcrypt.checkpw(password.encode(), hashed_password.encode())
+    
+    def login(self, mail, password):
+        user = user.get_user(mail)
+        if user and self.verify_password(password, user['password']):
+            return True
+        else:
+            return False
