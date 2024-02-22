a3d10c6002ca0319492e498a31368e06a21f92ab
erika-aschehoug <erika.aschehoug@laplateforme.io>
2024-02-22 14:57:22 +0100

    add function to hash the password

--- Class/Back/User.py
+++ Class/Back/User.py
@@ -1,12 +1,20 @@
 from Class.Back.Db import Db
+import hashlib
 
 class User:
     def __init__(self):
         self.db = Db(host="localhost", user="root", password="root", database="db_discord")
 
     def create_account(self, name, firstName, email, password):
+        def sha256_hash(choixmdp):              #hash fonction and in parameter password chosen
+            sha256 = hashlib.sha256()           
+            sha256.update(choixmdp.encode('utf-8'))
+            return sha256.hexdigest()
+        
+        hashed_password= sha256_hash(password)  #hash password before insert in data base     
+
         query = "INSERT INTO users (name, first_name, mail, password) VALUES (%s, %s, %s, %s)"  # The query to execute
-        values = (name, firstName, email, password)  # The values to insert
+        values = (name, firstName, email, hashed_password)  # The values to insert
         self.db.execute(query, values)
     
     def get_user(self, email):
