bf66d096fe588c9863452fe63af93e22e779d624
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2024-02-23 13:58:50 +0100

    creating Pictures package and adding pictures icon into it and into the Home_page.py

--- Class/Front/home_page.py
+++ Class/Front/home_page.py
@@ -16,8 +16,18 @@ class HomePage(tk.Frame):  # Creating a class StartPage which inherits from tk.T
         self.master.resizable(width=False, height=False)  # Making the window non-resizable
         self.master.configure(bg="darkblue")  # Setting the background color of the window
 
+        #picture loading and resizing
+        self.private_message_chat_icon = tk.PhotoImage(file="Class/Front/Pictures/private_message_chat_icon.png")
+        self.private_message_chat_icon = self.private_message_chat_icon.subsample(14, 14) # Resizing the image
+        self.message_chat_icon = tk.PhotoImage(file="Class/Front/Pictures/message_chat_icon.png")
+        self.message_chat_icon = self.message_chat_icon.subsample(22, 22) # Resizing the image
+        self.private_voice_chat_icon = tk.PhotoImage(file="Class/Front/Pictures/private_voice_chat_icon.png")
+        self.private_voice_chat_icon = self.private_voice_chat_icon.subsample(15, 15)  # Resizing the image
+        self.voice_chat_icon = tk.PhotoImage(file="Class/Front/Pictures/voice_chat_icon.png") 
+        self.voice_chat_icon = self.voice_chat_icon.subsample(15, 15)  # Resizing the image   
+
         # Creating and configuring a frame for the buttons field
-        self.left_frame = tk.Frame(master=frame, bg="DodgerBlue3", width=260, height=400)
+        self.left_frame = tk.Frame(master=frame, bg="DodgerBlue3", width=300, height=400)
         self.left_frame.pack(side="left")
         self.left_frame.place(x=50, y=250)
 
@@ -26,25 +36,42 @@ class HomePage(tk.Frame):  # Creating a class StartPage which inherits from tk.T
         self.public_voice_chat_button = tk.Button(master=frame, text="Salon Vocal Public", bg="RoyalBlue4", fg="white", width=20, height=1)
         self.public_voice_chat_button.pack()
         self.public_voice_chat_button.config(font=("Agency FB", 20, "italic"), relief="groove")
-        self.public_voice_chat_button.place(x=70, y=275)
+        self.public_voice_chat_button.place(x=110, y=275)
+
+        # Place the icons
+        self.voice_chat_icon_label = tk.Label(master=frame, image=self.voice_chat_icon, bg="DodgerBlue3")
+        self.voice_chat_icon_label.pack()
+        self.voice_chat_icon_label.place(x=50, y=275)
+    
+        self.private_voice_chat_icon_label = tk.Label(master=frame, image=self.private_voice_chat_icon, bg="DodgerBlue3")
+        self.private_voice_chat_icon_label.pack()
+        self.private_voice_chat_icon_label.place(x=50, y=365)
+
+        self.message_chat_icon_label = tk.Label(master=frame, image=self.message_chat_icon, bg="DodgerBlue3")
+        self.message_chat_icon_label.pack()
+        self.message_chat_icon_label.place(x=50, y=485)
+
+        self.private_message_chat_icon_label = tk.Label(master=frame, image=self.private_message_chat_icon, bg="DodgerBlue3")
+        self.private_message_chat_icon_label.pack()
+        self.private_message_chat_icon_label.place(x=50, y=575)
 
         # Creating and configuring the private voice chat button
         self.private_voice_chat_button = tk.Button(master=frame, text="Salon Vocal Privé", bg="RoyalBlue4", fg="white", width=20, height=1)
         self.private_voice_chat_button.pack()
         self.private_voice_chat_button.config(font=("Agency FB", 20, "italic"), relief="groove")
-        self.private_voice_chat_button.place(x=70, y=365)
+        self.private_voice_chat_button.place(x=110, y=365)
 
         # Creating and configuring the public text chat button
         self.public_text_chat_button = tk.Button(master=frame, text="Salon Message Public", bg="RoyalBlue4", fg="white", width=20, height=1)
         self.public_text_chat_button.pack()
         self.public_text_chat_button.config(font=("Agency FB", 20, "italic"), relief="groove")
-        self.public_text_chat_button.place(x=70, y=485)
+        self.public_text_chat_button.place(x=110, y=485)
 
         # Creating and configuring the private text chat button
         self.private_text_chat_button = tk.Button(master=frame, text="Salon Message Privé", bg="RoyalBlue4", fg="white", width=20, height=1)
         self.private_text_chat_button.pack()
         self.private_text_chat_button.config(font=("Agency FB", 20, "italic"), relief="groove")
-        self.private_text_chat_button.place(x=70, y=575)
+        self.private_text_chat_button.place(x=110, y=575)
 
 
 
